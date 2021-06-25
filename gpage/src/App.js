39953502c571a6356ab297d50b26d501c47783cc
Vishesh <kvishesh89@gmail.com>
2021-06-25 12:23:42 +0530

    add somehting

--- gpage/src/App.js
+++ gpage/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React. Dhinka chika.
         </a>
       </header>
     </div>
